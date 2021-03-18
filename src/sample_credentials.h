08ec145d662fe585aa865c2944deac8618f47541
Torsten Enkelmann <t.enkelmann@web.de>
2021-03-18 17:22:58 +0100

    Sample credentials.h

---
+++ src/sample_credentials.h
@@ -0,0 +1,9 @@
+//-------- Replace the following and save as credentials.h! -------
+#define WIFI_SSID "Your WiFi SSID"
+#define WIFI_PASS "Your WiFi Password"
+#define MQTT_SERVER "Your MQTT Server (IP or name)"
+#define MQTT_PORT 1883
+#define UPDATE_USER "The User for Updates"
+#define UPDATE_PASS "The Password for the Update User"
+#define MQTT_USER "The User for the MQTT Server"
+#define MQTT_PASS "The Password for the MQTT User"
